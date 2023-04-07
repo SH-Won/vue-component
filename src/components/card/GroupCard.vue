1f1c5c5b032141282a135d1bd9b428449caf5e6a
SH-Won <1317good@naver.com>
2023-04-07 17:27:18 +0900

    #1 card component & app header css

---
+++ src/components/card/GroupCard.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import Element from '../elements/Element.vue'
+
+withDefaults(
+  defineProps<{
+    imageUrl: string
+    label: string
+    isSub?: boolean
+  }>(),
+  {
+    imageUrl: '',
+    label: '',
+    isSub: false,
+  }
+)
+</script>
+
+<template>
+  <div class="group-card">
+    <Element v-if="isSub" name="Depth" :width="17" :height="34" />
+    <div class="card">
+      <img class="card-image" :src="imageUrl" />
+      <div class="card-explain">
+        <span>{{ label }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import '../../styles/colors.module.scss';
+@import '../../styles/typeface.module.scss';
+.group-card {
+  width: 90%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 14px;
+}
+.card {
+  width: 100%;
+  flex-grow: 1;
+  & > .card-image {
+    width: 80px;
+    height: 80px;
+  }
+  background-color: color('white');
+  display: flex;
+  gap: 30px;
+  padding: 16px;
+  border-radius: 8px;
+  align-items: center;
+  & > .card-explain {
+    @include typeface('Heading_3');
+  }
+}
+</style>
---
+++ src/components/card/index.ts
@@ -0,0 +1,2 @@
+export { default as Card } from './Card.vue'
+export { default as GroupCard } from './GroupCard.vue'
